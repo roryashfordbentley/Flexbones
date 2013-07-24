ac1210544347e77e1ceb298f118180a59d79d6c1
Ash Davies <ashley.davies@codebluegroup.co.uk>
2013-07-24 12:09:19 +0100

    Added basic search form template.

---
+++ searchform.php
@@ -0,0 +1,9 @@
+<?php /* The template for the Search form. */ ?>
+<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+    <div class="search-con">
+    	<div class="search-form">
+			<input type="text" value="" name="s" class="search-input" placeholder="Search">
+    		<input type="submit" value="Search" name="search-button" class="search-button">
+    	</div>
+    </div>
+</form> 
