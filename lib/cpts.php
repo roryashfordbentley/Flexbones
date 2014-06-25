aa8dce076567f14510d00ca2560a3511acd3a4f9
Ashley Davies <ash.davies@outlook.com>
2014-06-25 14:03:39 +0100

    Updated cpt function

--- lib/cpts.php
+++ lib/cpts.php
@@ -1,64 +1,58 @@
 <?php
 
 /**
- * Create a custom Post Type
+ * Add Custom Post Types to WordPress
  * Much better than using a plugin
  * @addedby Rory Ashford <rory@roikles.com>
  */
 
-// quickly add post types
-// * Be sure to use 'singular' $cpt_name *
-function add_cpt($cpt_name,$cpt_description,$dashicon,$has_archive = true){
-	register_post_type(
-		$cpt_name, array(
-			'label'             => $cpt_name,
-			'description'       => $cpt_description,
-			'public'            => true,
-			'menu_icon' 		=> $dashicon,
-			'show_ui'           => true,
-			'show_in_menu'      => true,
-			'capability_type'   => 'post',
-			'hierarchical'      => true,
-			'rewrite'           => array(
-				'slug'              => '',
-				'with_front'        => '0'
-			),
-			'query_var'      => true,
-			'has_archive'    => $has_archive,
-			'supports'       => array(
-				'title',
-				'editor',
-				'revisions',
-				'thumbnail',
-				'author',
-				'page-attributes',
-			),
-			'taxonomies'    => array(
-				'post_tag',
-			),
-			'labels'        => array(
-				'name'                => $cpt_name.'s',
-				'singular_name'       => $cpt_name,
-				'menu_name'           => $cpt_name.'s',
-				'add_new'             => 'Add ' . $cpt_name.'s',
-				'add_new_item'        => 'Add New ' . $cpt_name,
-				'edit'                => 'Edit ',
-				'edit_item'           => 'Edit ' . $cpt_name,
-				'new_item'            => 'New ' . $cpt_name,
-				'view'                => 'View ' . $cpt_name.'s',
-				'view_item'           => 'View ' . $cpt_name,
-				'search_items'        => 'Search ' . $cpt_name.'s',
-				'not_found'           => 'No ' . $cpt_name .'s Found',
-				'not_found_in_trash'  => 'No ' . $cpt_name .'s Found in Trash',
-				'parent'       		  => 'Parent ' . $cpt_name
-			),
-		)
+//add_action('init', 'flexbones_cpts');
+
+function flexbones_cpts() {
+	$post_type_name = "";
+	$single_name 	= "";
+	$plural_name 	= "";
+	$icon 			= "";
+
+	$post_type_options = array(
+		'label'            		=> $single_name,
+		'description'       	=> $description,
+		'public'            		=> true,
+		'menu_icon' 		=> 'dashicons-book',
+		'show_ui'          	=> true,
+		'show_in_menu'      => true,
+		'capability_type'   	=> 'post',
+		'hierarchical'      	=> true,
+		'rewrite'           		=> array(
+			'slug'              		=> '',
+			'with_front'        	=> '0'
+		),
+		'query_var'      		=> true,
+		'has_archive'    		=> true,
+		'supports'       		=> array(
+			'title',
+			'editor',
+			'revisions',
+			'thumbnail',
+			'author',
+			'page-attributes',
+		),
+		'labels'        		=> array(
+			'name'              	=> $single_name,
+			'singular_name'     	=> 'Service',
+			'menu_name'         	=> 'Service',
+			'add_new'             	=> 'Add Service',
+			'add_new_item'     	=> 'Add New Service',
+			'edit'                		=> 'Edit',
+			'edit_item'           	=> 'Edit Service',
+			'new_item'            	=> 'New '. $single_name,
+			'view'                	=> 'View Service',
+			'view_item'           	=> 'View Service',
+			'search_items'        => 'Search Service',
+			'not_found'           	=> 'No '. $single_name . 'Found',
+			'not_found_in_trash'  => 'No Services Found in Trash',
+			'parent'       		=> 'Parent '. $single_name
+		),
 	);
+	register_post_type($post_type_name, $post_type_options);
 }
-
-// Full list of dashicons: http://melchoyce.github.io/dashicons/
-
-//add_cpt('singlular_name','cpt_description','icon','has_archive');
-
-
-
