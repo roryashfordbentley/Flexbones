33c86473cf230fdc2d88ae534e3c3ff48c19217d
Rory Ashford <rory@roikles.com>
2015-04-28 15:08:40 +0100

    Added new tools!! (responsive typography mixin)

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
     <!-- Content!! -->
+    <div class="wrapper">
+        <div class="grid">
+            <div class="span--1-1  span--1-3--m">
+                <p>This is a paragraph</p>
+            </div>
+            <div class="span--1-1  span--1-3--m">
+                <p>This is a paragraph</p>
+            </div>
+            <div class="span--1-1  span--1-3--m">
+                <p>This is a paragraph</p>
+            </div>
+        </div>
+    </div>
 <?php get_footer(); ?>
