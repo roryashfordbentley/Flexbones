8ffccd22e10ad77c738e6e701a70f8206f72b5f2
Rory Ashford <rory@roikles.com>
2015-12-22 10:16:23 +0000

    Added admin stylesheet and enqued it. Also added some basic css for the wp-admin customizer.

--- lib/theme-setup.php
+++ lib/theme-setup.php
@@ -17,8 +17,20 @@ function flexbones_setup() {
     register_nav_menus(array(
         'main_menu' => 'main menu'
     ));
-    
+
     register_sidebar(array('id'=>'sidebar-1')); 
 }
 
 add_action('after_setup_theme', 'flexbones_setup');
+
+/**
+ * Admin CSS
+ *
+ * Enque an additional admin stylesheet
+ */
+
+add_action('admin_enqueue_scripts', 'admin_css');
+function admin_css() {
+    wp_enqueue_style('admin-css', get_template_directory_uri() . '/style-admin.css');
+}
+
