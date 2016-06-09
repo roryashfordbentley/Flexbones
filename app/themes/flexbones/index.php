e030d0aaa8fb076badb04f83469258c7cc7e1335
Rory Ashford <labs@codebluedigital.com>
2016-06-09 16:02:45 +0100

    added debug log to .gitignore

--- app/themes/flexbones/index.php
+++ app/themes/flexbones/index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-    <?php //bem_menu('main_menu','main menu'); ?>
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <?php the_content(); ?>
     <?php endwhile; endif; ?>
