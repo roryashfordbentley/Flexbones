5e0c792e2c4be7eab057031a010c7324055adbd0
Rory Ashford <labs@codebluedigital.com>
2016-06-10 12:02:27 +0100

    Working version of the component structure!

--- app/themes/flexbones/index.php
+++ app/themes/flexbones/index.php
@@ -1,6 +1,11 @@
-<?php //get_header(); ?>
-<?php get_component('header') ?>
-    <?php if(have_posts()): while(have_posts()): the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile; endif; ?>
+<?php get_header(); ?>
+
+<?php get_component('header'); ?>
+
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <?php the_content(); ?>
+<?php endwhile; endif; ?>
+
+<?php get_component('footer'); ?>
+
 <?php get_footer(); ?>
--- app/themes/flexbones/lib/theme-setup.php
+++ app/themes/flexbones/lib/theme-setup.php
@@ -36,10 +36,27 @@ function admin_css() {
 
 /**
  * get components
+ *
+ * checks that a component exists and returns the component
+ * if it exists.
+ *
  */
+
 function get_component($component){
 
-    require_once( get_stylesheet_directory_uri() . '/components/' . $component . '/index.php');
+    if(empty($component)){
+        echo '<div style="background: #f00; color: white;"><strong>Missing Argument.</strong> Pass the name of the component folder e.g. "header"</div>';
+        return false;
+    }
+
+    $component_path = get_stylesheet_directory() . '/components/' . $component . '/index.php';
+
+    if(!file_exists($component_path) ){
+        echo '<div style="background: #f00; color: white;"><strong>Component '. $component .' does not exist.</strong> Check the name and path are correct. Path = '. $component_path .'</div>';
+        return false;
+    }
+
+    require_once( get_stylesheet_directory() . '/components/' . $component . '/index.php');
 
     return true;
 }
