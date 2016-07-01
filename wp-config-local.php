12a68e5a8a92e90c60cea5ae5ffcfa4c1857b198
Rory Ashford <rory@superstrukt.co.uk>
2016-07-01 15:27:13 +0100

    removed uploads define from config

--- wp-config-local.php
+++ wp-config-local.php
@@ -9,7 +9,6 @@ define("WP_DEBUG_DISPLAY", false);
 define("WP_DEBUG_LOG", true);
 define("WP_CONTENT_DIR", dirname(__FILE__). "/app" );
 define("WP_CONTENT_URL", "http://". $_SERVER["HTTP_HOST"]. "/flexbones/app");
-define("UPLOADS", "app/uploads");
 define("WP_HOME","http://localhost/flexbones");
 define("WP_SITEURL","http://localhost/flexbones/wp");
 define("WPLANG", "en_GB");
