0df3cb02c48c181e2ee53129db4110ff60009973
Rory Ashford <rory@roikles.com>
2016-12-08 11:50:49 +0000

    cleaned up index.php

--- app/themes/flexbones/index.php
+++ app/themes/flexbones/index.php
@@ -1,13 +1,7 @@
 <?php get_header(); ?>
 
-<?php get_component('header'); ?>
-<div class="wrapper  wrapper--small">
-    <a href="#">Links</a>
-</div>
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <?php the_content(); ?>
 <?php endwhile; endif; ?>
-
-<?php get_component('footer'); ?>
 
 <?php get_footer(); ?>
